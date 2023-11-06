e285a60e6aff2be97e002240f948eb67217e7ed1
Hims-21 <himansugami53054@gmail.com>
2023-11-06 16:24:45 +0530

    configure tailwind in react project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import Card from "./components/card";
 function App() {
-  const [count, setCount] = useState(0)
-
+  let object1 = {
+    userName: "Himansu Gami",
+    password: "Himansu@21",
+  };
+  let object2 = {
+    userName: "Disha Gami",
+    password: "Disha@16",
+  };
+  let myArr = [1, 2, 3];
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="bg-green-400 text-black rounded-xl p-4 mb-4">
+        TailwindTest
+      </h1>
+      <Card
+        name="Himanshu Gami"
+        myObj={object1}
+        arr={myArr}
+        btnText="Himanshu"
+      />
+      <Card myObj={object2} btnText="Dishu" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
